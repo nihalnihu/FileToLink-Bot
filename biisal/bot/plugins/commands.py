455b5044f56cce98403c845028f702db4e524d59
darkhacker34 <72502160+darkhacker34@users.noreply.github.com>
2024-02-24 09:09:01 +0530

    Update commands.py

--- biisal/bot/plugins/commands.py
+++ biisal/bot/plugins/commands.py
@@ -13,7 +13,7 @@ from pyrogram.errors import UserNotParticipant
 from biisal.utils.file_properties import get_name, get_hash, get_media_file_size
 db = Database(Var.DATABASE_URL, Var.name)
 from pyrogram.types import ReplyKeyboardMarkup
-from biisal.vars import bot_name , bisal_channel , bisal_grp
+from biisal.vars import bot_name , bisal_channel , bisal_grp , nihal_cannel
 
 
 SRT_TXT = """<b>𝙷𝚢 {} ㋛︎!,
