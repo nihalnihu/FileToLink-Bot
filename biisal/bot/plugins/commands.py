150f46c2571d29010da8336d244e4797a20a9896
darkhacker34 <72502160+darkhacker34@users.noreply.github.com>
2024-02-24 10:43:05 +0530

    Update commands.py

--- biisal/bot/plugins/commands.py
+++ biisal/bot/plugins/commands.py
@@ -245,7 +245,7 @@ async def cb_handler(client, query):
     elif data == "aboutDev":
         # please don't steal credit
         await query.message.edit_caption(
-            caption=f"<b>Hy ᴅᴇᴀʀ 🥰, ɪ,ᴀᴍ ᴄʀᴇᴀᴛᴇᴅ ᴛʜɪᴤ ʙᴏᴛ\n\n𝙒𝙝𝙖𝙩𝙨𝙖𝙥𝙥 𝙈𝙤𝙫𝙞𝙚𝙨 𝘾𝙝𝙖𝙣𝙣𝙚𝙡 : <a href='https://whatsapp.com/channel/0029VaLe9dSGk1G2sHAqbR0s'>ＭＶ_ＲＯＣＫＥＲＳ</a> \n\n𝙂𝙞𝙩𝙃𝙐𝘽 : <a href='https://github.com/darkhacker34'>ＤＡＲＫ ＨＡＣＫＥＲ</a> \n\n <a href='https://www.instagram.com/nihh____al'>ＮＩＨＡＬ</a></b>",
+            caption=f"<b>Hy ᴅᴇᴀʀ 🥰, ᴍʏ ɴᴀᴍᴇ ɪs <a href='https://t.me/nihh_all'>ＮＩＨＡＬ</a>. ɪ,ᴀᴍ ᴄʀᴇᴀᴛᴇᴅ ᴛʜɪᴤ ʙᴏᴛ\n\n𝙒𝙝𝙖𝙩𝙨𝙖𝙥𝙥 𝘾𝙝𝙖𝙣𝙣𝙚𝙡 : <a href='https://whatsapp.com/channel/0029VaLe9dSGk1G2sHAqbR0s'>𝗠𝗩 𝗥𝗢𝗖𝗞𝗘𝗥𝗦</a> \n\n𝙂𝙞𝙩𝙃𝙐𝘽 : <a href='https://github.com/darkhacker34'>ＤＡＲＫ ＨＡＣＫＥＲ</a></b>",
             reply_markup=InlineKeyboardMarkup(
                 [[ 
                      InlineKeyboardButton("ʜᴏᴍᴇ", callback_data="start"),
