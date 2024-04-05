b9585e5cdc2a44993240fbb043e782ca2e32348b
darkhacker34 <72502160+darkhacker34@users.noreply.github.com>
2024-04-05 06:32:35 +0530

    Update stream.py

--- biisal/bot/plugins/stream.py
+++ biisal/bot/plugins/stream.py
@@ -88,8 +88,12 @@ async def private_receive_handler(c: Client, m: Message):
             text=msg_text.format(get_name(log_msg), humanbytes(get_media_file_size(m)), online_link, stream_link),
             quote=True,
             disable_web_page_preview=True,
-            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("🎭 ᴡᴀᴛᴄʜ", url=stream_link),
-                                                InlineKeyboardButton('📥 ᴅᴏᴡɴʟᴏᴀᴅ', url=online_link)]])
+            reply_markup=InlineKeyboardMarkup([InlineKeyboardButton("🎭 ᴡᴀᴛᴄʜ", url=stream_link),
+                                                InlineKeyboardButton('📥 ᴅᴏᴡɴʟᴏᴀᴅ', url=online_link),
+                                               ],
+                                               
+                                               [InlineKeyboardButton("coffee", url="https://core.telegram.org/bots/api")],
+                                               )
         
         )
     except FloodWait as e:
